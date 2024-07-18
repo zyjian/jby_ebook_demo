a5eb52779ced739e3817f054c6377ca7785a5ec8
zhu <719307805@qq.com>
2024-07-18 15:39:43 +0800

    feat:文档更新

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# jby_ebook_demo
+# 京版云 电子课本Android sdk  集成说明
+
+## 简介
+京版云电子课本Android sdk 是京版云提供的android sdk，可以方便的在android项目中集成电子课本功能
+
+## 集成步骤
+
+#### 1. 引入sdk aar 
+     把 libs/jbyEbook-release.aar 添加到你的项目中(位置 app目录下)
+
+#### 2. 添加依赖
+    setting->project structure->app->dependencies->+->file dependency->选择libs/jbyEbook-release.aar
+
+#### 3 资源列表接口，集成方可以根据自己的需求自定义该列显示页面UI
+    https://new.jingbanyun.com/api/Booktext/textbook_list?token=3aadbfcb4d45f55338fed3df7e02b79c1030ab18774de427777c8a13a330ae6a
+
+
+#### 4 打开电子课本页面
+```
+Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("id", "4508");
+                startActivity(intent);
+                
+//这里的 是课程id, 示例中的id 是从资源列表接口中获取的
+                
+```
+
+
+    
+
+
+
--- app/src/main/java/com/jtcloud/jby_ebook_demo/MainActivity.java
+++ app/src/main/java/com/jtcloud/jby_ebook_demo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.jtcloud.jby_ebook_demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.jtcloud.jbyebook.WebViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        // 找到按钮
+        Button myButton = findViewById(R.id.button);
+
+        // 设置点击事件监听器
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 在这里处理按钮点击事件
+//                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("id", "4508");
+                startActivity(intent);
+
+            }
         });
     }
 }
